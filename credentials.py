8a181f31a21697f6b1d7de9f65e963bdd5725b80
VBTCPMRed2019 <46924034+VBTCPMRed2019@users.noreply.github.com>
2019-04-12 13:17:36 -0400

    Update credentials.py
    Added the keys

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-consumer_key = ''
-consumer_secret = ''
-access_token = ''
-access_token_secret = ''
+consumer_key = 'uQWDVIjEwCdyxFboHuUr1L5xy'
+consumer_secret = '0NP2y2PuvHgD1qsibkrJEDd5WH6u3z96qGf8cpEhNE85POWDN3'
+access_token = '1115308641169625093-H5nZt9ZEVA3qRtdyLfg5jD4Yzto6t7'
+access_token_secret = 'UcgE5WcG2WZRDdub9INqEJbRZomIvnB2SwAZa5HodXDYQ'
