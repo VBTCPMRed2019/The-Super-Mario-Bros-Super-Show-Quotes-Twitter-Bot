498469ad5c682406d84c642e29b89c59de668170
VBTCPMRed2019 <46924034+VBTCPMRed2019@users.noreply.github.com>
2019-04-17 13:08:58 -0400

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,3 +1,4 @@
+#The four access keys fort he SMBSS Quote Bot, they allow the bot program to access the twitter account.
 consumer_key = 'uQWDVIjEwCdyxFboHuUr1L5xy'
 consumer_secret = '0NP2y2PuvHgD1qsibkrJEDd5WH6u3z96qGf8cpEhNE85POWDN3'
 access_token = '1115308641169625093-H5nZt9ZEVA3qRtdyLfg5jD4Yzto6t7'
