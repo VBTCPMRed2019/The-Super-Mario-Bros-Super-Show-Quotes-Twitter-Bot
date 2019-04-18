8b0feb428f04a44281c02ac5ab498c12cf450a42
VBTCPMRed2019 <46924034+VBTCPMRed2019@users.noreply.github.com>
2019-04-18 12:51:44 -0400

    Update 4/18/2019
    Added a better way to create and clean the file.

--- SuperShowBot.py
+++ SuperShowBot.py
@@ -10,16 +10,22 @@ authority = tweepy.OAuthHandler(consumer_key, consumer_secret)
 authority.set_access_token(access_token, access_token_secret)
 api = tweepy.API(authority, wait_on_rate_limit = True)
 
+def ClearFile(File): #Erases the content of the File, is only called when there are more than 230 quotes in "UsedQuotes.txt"
+    File.seek(0)
+    File.truncate()
+    
+File = open("UsedQuotes.txt", "w+") #Opens "UsedQuotes.txt" or creates it if it does not exist
+
 # Perpetual loop where it waits for certain times to send a tweet/update its status
 while True:
     time = datetime.datetime.now()
-    if time.hour == 11 and time.minute == 59: # 11:59 the bot's text file is cleared so it can reuse quotes it used the pervious day. 
-        os.remove('UsedQuotes.txt')
-        File = open("UsedQuotes.txt", "w+")
+    line_count = len(open("UsedQuotes.txt").readlines())
     if (time.hour == 24 or time.hour == 4 or time.hour == 8 or time.hour == 12 or time.hour == 16 or time.hour == 20) and (time.minute == 0 and time.second < 59): 
         message = QuoteMod.GimmeQuote()
         print(message)
         api.update_status(message)
+        if line_count > 230: 
+            ClearFile(File)
         wait(80)
         
 # A future idea where the bot will respond when it is @'d
